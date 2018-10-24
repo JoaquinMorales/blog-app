8a07ac766bd784b82fa7eb853f0c97f8222a8f99
Joaquin Morales Andrade <joaquin.morales@avantica.net>
2018-10-24 17:18:31 -0400

    correcting lint issue with users details tests

--- src/views/UserDetails/UserDetails.test.js
+++ src/views/UserDetails/UserDetails.test.js
@@ -1,31 +1,31 @@
-import React from "react";
-import { shallow } from "enzyme";
-import UserDetails from "./UserDetails";
+import React from 'react';
+import { shallow } from 'enzyme';
+import UserDetails from './UserDetails';
 
-describe("User details view", () => {
+describe('User details view', () => {
   const context = {};
   beforeAll(() => {
     const userQuery = shallow(
-      <UserDetails location={{ pathname: "/users/1" }} />
+      <UserDetails location={{ pathname: '/users/1' }} />,
     )
-      .find("Query")
+      .find('Query')
       .at(0)
-      .prop("children");
+      .prop('children');
     context.userDetails = shallow(
-      <div>{userQuery({}, { loading: false })}</div>
+      <div>{userQuery({}, { loading: false })}</div>,
     );
   });
-  it("should render a form with 6 fields", () => {
+  it('should render a form with 6 fields', () => {
     const { userDetails } = context;
-    expect(userDetails.find("FormField")).toHaveLength(6);
+    expect(userDetails.find('FormField')).toHaveLength(6);
     userDetails.unmount();
   });
 
-  it("should render a table with posts made by some user", () => {
+  it('should render a table with posts made by some user', () => {
     const { userDetails } = context;
-    const postQuery = userDetails.find("Query").prop("children");
+    const postQuery = userDetails.find('Query').prop('children');
     const posts = shallow(<div>{postQuery({}, { loading: false })}</div>);
-    expect(posts.find("DataTable")).toHaveLength(1);
+    expect(posts.find('DataTable')).toHaveLength(1);
     posts.unmount();
   });
 });
